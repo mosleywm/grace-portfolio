741dd5ba52b56c85000b51d1354df226d5ab8f50
William Mosley <mosleywm@gmail.com>
2018-01-18 14:38:43 -0800

    Update about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,19 +5,27 @@ import headshot from '../assets/grace_headshot.jpg';
 import SectionHeader from '../components/section-header';
 
 const styles = createStyleSheet(theme => ({
+  container: {
+    display: 'flex',
+    'flex-direction': 'column',
+    'justify-items': 'center',
+    'align-items': 'center',
+    '@media(min-width: 420px)': {
+      'flex-direction': 'row'
+    }
+  },
   portrait: {
     width: '100%',
     position: 'relative',
     top: '-25px'
   },
   portraitContainer: {
+    flex: '0 0 auto',
     width: '200px',
     height: '200px',
     overflow: 'hidden',
     'border-radius': '100px',
-    margin: 'auto',
     '@media(min-width: 420px)': {
-      float: 'left',
       margin: '0 20px 20px 0'
     }
   },
@@ -41,13 +49,13 @@ class About extends Component {
     return (
       <div id="about" className={classes.section}>
         <SectionHeader title="About" />
-        <div className={classes.portraitContainer}>
-          <img className={classes.portrait} src={headshot} alt={"Grace Pastore"} />
-        </div>
-        <div className={classes.bio}>
-          <p>I've been a writer for as long as I can remember - but not always in the classic sense. I wasn't penning novellas at age eight (maybe a terrible poem or two). Instead, writing has always been an intrinsic part of how I problem-solve and communicate - my constant obsession, a running commentary of my personal and professional development. The common thread was, and continues to be, curiosity.</p>
-          <p>Regardless of the product I'm selling, the idea I'm pitching or the answer I'm formulating, I've found that curiosity is more than a bonus, but a requirement for versatile writing and quality project management. In each of my jobs, from the more tactically-focused to the creative, I've delivered polished finished products, but I've never done so alone. I ask questions, set meetings, contribute to design, document processes and solicit feedback. All take curiosity.</p>
-          <p>My professional experience, in both technical and creative settings, has given me a respect for the analytical side of writing and marketing, as well as a reverence for the aesthetics. I plan to keep learning, asking questions and extending myself to produce compelling writing and optimal outcomes. So, hi! I'm Grace - a Virginia-native and an enthusiastic baker, artist, sardonic British comedy addict and writer.</p>
+        <div className={classes.container}>
+          <div className={classes.portraitContainer}>
+            <img className={classes.portrait} src={headshot} alt={"Grace Pastore"} />
+          </div>
+          <div className={classes.bio}>
+            <p>Writer, editor and marketing professional based in Seattle, WA.</p>
+          </div>
         </div>
       </div>
     );
